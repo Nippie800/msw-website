f3ba4f63e6c50e794b47dcf97168e4386eca5c41
Nippie800 <hitupdon@gmail.com>
2026-06-01 13:08:06 +0200

    update dropbelt

--- components/DropBelt.tsx
+++ components/DropBelt.tsx
@@ -20,34 +20,61 @@ export default function DropBelt({
   const beltProducts = [
     ...products,
     ...products,
+    ...products,
   ];
 
   return (
-    <div className="relative w-full overflow-hidden py-10 md:py-16">
+    <div className="relative py-10 md:py-16">
 
-      {/* LEFT FADE */}
-      <div className="pointer-events-none absolute left-0 top-0 z-10 h-full w-12 bg-gradient-to-r from-black to-transparent md:w-36" />
+      {/* DESKTOP RUNWAY */}
+      <div className="relative hidden overflow-hidden md:block">
 
-      {/* RIGHT FADE */}
-      <div className="pointer-events-none absolute right-0 top-0 z-10 h-full w-12 bg-gradient-to-l from-black to-transparent md:w-36" />
+        {/* LEFT FADE */}
+        <div className="pointer-events-none absolute left-0 top-0 z-10 h-full w-36 bg-gradient-to-r from-black to-transparent" />
 
-      <div className="group w-full overflow-hidden">
-        <div className="marquee-track group-hover:[animation-play-state:paused]">
+        {/* RIGHT FADE */}
+        <div className="pointer-events-none absolute right-0 top-0 z-10 h-full w-36 bg-gradient-to-l from-black to-transparent" />
 
-          {beltProducts.map(
-            (product, index) => (
-              <div
-                key={`${product.id}-${index}`}
-                className="mr-8 inline-block align-top sm:mr-12 md:mr-20"
-              >
-                <ProductCard
-                  product={product}
-                />
-              </div>
-            )
-          )}
+        <div className="group w-full overflow-hidden">
+          <div className="marquee-track group-hover:[animation-play-state:paused]">
 
+            {beltProducts.map(
+              (product, index) => (
+                <div
+                  key={`${product.id}-${index}`}
+                  className="mr-20 inline-block align-top"
+                >
+                  <ProductCard product={product} />
+                </div>
+              )
+            )}
+
+          </div>
         </div>
+      </div>
+
+      {/* MOBILE RUNWAY */}
+      <div
+        className="
+          flex
+          gap-6
+          overflow-x-auto
+          px-1
+          pb-4
+          md:hidden
+          snap-x
+          snap-mandatory
+          scrollbar-hide
+        "
+      >
+        {products.map((product) => (
+          <div
+            key={product.id}
+            className="snap-center flex-shrink-0"
+          >
+            <ProductCard product={product} />
+          </div>
+        ))}
       </div>
 
     </div>
